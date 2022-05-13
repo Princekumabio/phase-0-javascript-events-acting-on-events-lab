ed69e3ede48a8e10329a080db7916f17c90a34f8
kumabio <kumabio.prince@student.moringaschool.com>
2022-05-13 18:30:11 +0000

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+/*document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerRight();
+    }
+  });
+*/
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4"
+
+dodger.style.bottom = "100px";
+
+dodger.style.bottom = "0px";
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keyup", function (x) {
+  if (x.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
